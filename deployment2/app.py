7431670508a7630fffc1829ee8d1c9eb95799c85
NAYAN BHANSALI <nayanbhansali22@gmail.com>
2024-08-07 16:49:53 +0530

    Add files via upload

--- deployment2/app.py
+++ deployment2/app.py
@@ -56,4 +56,6 @@ def index():
     return render_template('index.html', prediction=prediction, prob_df=prob_df)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=8080, debug=True)
+
